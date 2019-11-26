f82130545e37f30569842ccfd74b5670cc40e009
Dan Boehmke <Daniel@webappclouds.com>
2019-11-26 14:23:43 -0500

    Checking for online

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://Root:1234@vidjot-prod-owrdr.mongodb.net/'}
+  module.exports = {mongoURI: 'mongodb+srv://Root:1234@vidjot-prod-owrdr.mongodb.net/test?retryWrites=true&w=majority'}
+  console.log("Online");
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+  console.log("Local");
 }
