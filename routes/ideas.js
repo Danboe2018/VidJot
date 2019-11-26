54844f5b2057149e8fceecc0eb5fdc3ebad05b3d
Dan Boehmke <Daniel@webappclouds.com>
2019-11-26 12:52:07 -0500

    Complete application

--- routes/ideas.js
+++ routes/ideas.js
@@ -10,7 +10,7 @@ const Idea = mongoose.model('ideas');
 
 // Idea Index Page
 router.get('/', ensureAuthenticated, (req, res) => {
-  Idea.find({})
+  Idea.find({user: req.user.id})
     .sort({ date: 'desc' })
     .then(ideas => {
       res.render('ideas/index', {
@@ -30,6 +30,10 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     _id: req.params.id
   })
     .then(idea => {
+      if(idea.user != req.user.id){
+        req.flash('error_msg', 'Not Authorized');
+        res.redirect('/ideas');
+      }
       res.render('ideas/edit', {
         idea: idea
       });
@@ -56,7 +60,8 @@ router.post('/', ensureAuthenticated, (req, res) => {
   } else {
     const newUser = {
       title: req.body.title,
-      details: req.body.details
+      details: req.body.details,
+      user: req.user.id
     }
     new Idea(newUser)
       .save()
